7c5331fb241cb07f46919d1e296339b452514c00
Thelsandro <39775561+thelsandroantunes@users.noreply.github.com>
2019-12-11 10:26:53 -0400

    loop wordpress

--- curso/page-home.php
+++ curso/page-home.php
@@ -9,7 +9,30 @@
 		</section>
 		<section class="services">
 			<div class="container">
-				<div class="row">Serviços</div>
+				<h1>Our Services</h1>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="services-item">
+						<?php if(is_active_sidebar('services-1')){
+							dynamic_sidebar('services-1');
+						} ?>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="services-item">
+						<?php if(is_active_sidebar('services-2')){
+							dynamic_sidebar('services-2');
+						} ?>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="services-item">
+						<?php if(is_active_sidebar('services-3')){
+							dynamic_sidebar('services-3');
+						} ?>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 		<section class="middle-area">
@@ -17,23 +40,49 @@
 				<div class="row">
 					<?php get_sidebar('home'); ?>
 					<div class="news col-md-8">
-						<?php
-						//Se houver algum post
-						if (have_posts()):
-							//Enquanto houver posts, mostre-os pra gente
-							while( have_posts()): the_post();
-						?>
-                        
-                        <p>teste teste teste</p>
+						<div class="container">
+							<h1>Latest News</h1>
+							<div class="row">
+							<?php 
+								//Primeiro Loop
+								$featured = new WP_QUERY( 'post_type=post&post_per_page=1&cat=3,6' );
 
-						<?php
-							endwhile;
-						else:
-						?>
+								if ($featured->have_posts()):
+									while($featured->have_posts()): $featured->the_post();
+							?>
+							<div class="col-12">
+							<?php get_template_part('template-parts/content', 'featured'); ?>
+							</div>
+							<?php
+									endwhile;
+									wp_reset_postdata();
+								endif;
+							
+								//Segundo Loop
+								$args = array(
+									'post_type' => 'post',
+									'posts_per_page' => 2,
+									'category__note_in' => array(5),
+									'category__in' => array(3,6),
+									'offset' => 2
+								);
 
-						<p>There's nothing yet to be displayed...</p>
-						
-						<?php endif; ?>
+								$secondary = new WP_QUERY( $args );
+
+								if ($secondary->have_posts()):
+									while($secondary->have_posts()): $secondary->the_post();
+							?>
+							<div class="col-sm-6">
+							<?php get_template_part('template-parts/content', 'secondary'); ?>
+							</div>
+							<?php
+									endwhile;
+									wp_reset_postdata();
+								endif;
+							?>
+
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
---
+++ curso/template-parts/content-featured.php
@@ -0,0 +1,14 @@
+<article <?php post_class(array('class' => 'featured')); ?>>
+    <h2><?php the_title(); ?></h2>	
+    <div class="thumbnail"></div>
+    <?php the_post_thumbnail('large', array('class'=>'img-fluid')); ?>
+    <div class="meta-info">
+    <p> 
+        by  <span><?php the_author_posts_link(); ?></span>
+        Categories: <span><?php the_category(' '); ?> </span>
+        <?php the_tags( 'Tags: <span>', ', ', '</span>' ); ?>
+    </p>
+    <p><span><?php echo get_the_date(); ?></span></p>
+    </div>
+    <?php the_excerpt(); ?>
+</article>
